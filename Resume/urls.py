20bdd8c8d45c9f23e452d416001410b4e6f7cd19
Ronak Kanthariya <76777057+Ronak0009@users.noreply.github.com>
2021-07-31 09:09:29 +0530

    Update urls.py

--- Resume/urls.py
+++ Resume/urls.py
@@ -31,8 +31,8 @@ import home
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
-    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
